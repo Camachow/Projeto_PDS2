79f39a0ca9ccff23495d3f4238e85cc188da054d
DiegoChagas11 <diegao.c@hotmail.com>
2022-06-23 10:41:34 -0300

    inseri minhas classes

---
+++ include/Supermercado.hpp
@@ -0,0 +1,25 @@
+#ifndef SUPERMERCADO_H
+#define SUPERMERCADO_H
+
+#include <vector> 
+#include "Produto.hpp"
+
+using namespace std;
+
+class Supermercado{
+    
+    private:
+      vector<Produto> _lista_produtos;
+      float _saldo_caixa;
+      vector<Produto> _lista_promocional;
+
+    public:
+      
+      float retorna_saldo_caixa();
+      void produto_comprado();
+      void insere_produto(Produto p);
+      void imprime_lista_produtos();
+
+};
+
+#endif
---
+++ src/Gerente.cpp
@@ -0,0 +1,16 @@
+#include "Gerente.hpp"
+#include "Supermercado.hpp"
+
+#include <string>
+
+using namespace std;
+
+
+Gerente::Gerente(string nome, Supermercado s){
+    _nome= nome;
+    _supermercado_referente= s;
+}
+
+float Gerente::consulta_saldo_caixa(){
+    return _supermercado_referente.retorna_saldo_caixa();
+}
