9a5f408c9a30f22a2e425bd3cc48ac059238016f
Daniel Nogueira Junqueira <danielnjunq@gmail.com>
2022-07-05 01:08:34 -0300

    adicionei alguns métodos na função main

--- main.d
+++ main.d
@@ -1,3 +1,5 @@
 obj/main.o: src/main.cpp src/../include/caixa.h \
  src/../include/funcionario.h src/../include/cliente.h \
- src/../include/supermercado.h src/../include/produto.h
+ src/../include/supermercado.h src/../include/produto.h \
+ src/../include/gerente.h src/../include/supermercado.h \
+ src/../include/produto.h
--- src/supermercado.cpp
+++ src/supermercado.cpp
@@ -13,41 +13,35 @@ void Supermercado::insere_produto(Produto p){
     _lista_produtos.push_back(p);
 }
 
- void Supermercado::imprime_lista_produtos(){
+void Supermercado::imprime_lista_produtos(){
     for(Produto p: _lista_produtos){
         cout << p.retorna_nome() << endl;
     }
 } 
 
 void Supermercado::imprime_lista_promocional(){
-  for(Produto p: _lista_promocional){
-    cout << p.retorna_nome() << endl;
+    for(Produto p: _lista_promocional){
+        cout << p.retorna_nome() << endl;
     }
 }
 
 void Supermercado::produto_comprado(int qnt, string nome_produto, float preco){
-
-if(_lista_produtos.empty()==false){ 
-  
-  for(auto it=_lista_produtos.begin(); it<=_lista_produtos.end(); it++){
-    if(it->retorna_nome() == nome_produto){
-      it->atualiza_quantidade(qnt);
-      _saldo_caixa += qnt*(it->retorna_preco());
+  if(_lista_produtos.empty() == false){ 
+    for(auto it=_lista_produtos.begin(); it <=_lista_produtos.end(); it++){
+      if(it->retorna_nome() == nome_produto){
+        it->atualiza_quantidade(qnt);
+        _saldo_caixa += qnt*(it->retorna_preco());
+      }
     }
   }
-  
-}
-if(_lista_promocional.empty()==false){
- 
-  for(auto 
- it=_lista_promocional.begin();it<=_lista_promocional.end();it++){
-    if(it->retorna_nome() == nome_produto){
-      it->atualiza_quantidade(qnt);
-      _saldo_caixa += qnt*(it->retorna_preco());
+  if(_lista_promocional.empty() == false){
+      for(auto it=_lista_promocional.begin(); it <=_lista_promocional.end(); it++){
+      if(it->retorna_nome() == nome_produto){
+        it->atualiza_quantidade(qnt);
+        _saldo_caixa += qnt*(it->retorna_preco());
+      }
     }
   }
-
-}
   
 }// se as listas estiverem vazias, essa funcao nao faz nada
  //se nao tiver esses "if" verificando se elas estao vazias,
