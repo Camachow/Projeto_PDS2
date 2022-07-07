7bb6c7faa2d4b40987c516ecc7a07128dc630c8c
Rafael Camacho <rafaelcamachofitti15@gmail.com>
2022-07-07 13:09:14 -0300

    Tirei lista de produtos e promoções da classe cliente

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -4,14 +4,6 @@ Cliente::Cliente(string nome) {
     _nome = nome;
 }
 
-void Cliente::ver_lista_produtos(Supermercado s) {
-    s.imprime_lista_produtos();
-}
-
-void Cliente::ver_lista_promocional(Supermercado s) {
-    s.imprime_lista_promocional();
-}
-
 void Cliente::comprar_produto(Produto p, Supermercado *s, int quantidade) {
     s->produto_comprado(quantidade, p.retorna_nome(), p.retorna_preco());
 }
