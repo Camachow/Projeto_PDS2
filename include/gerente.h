3d4a541063fd621bf003c9d9f46a0faf7c0ebb60
Rafael Camacho <rafaelcamachofitti15@gmail.com>
2022-07-18 00:49:01 -0300

    comentarios todos funcionando

--- include/gerente.h
+++ include/gerente.h
@@ -16,31 +16,31 @@ class Gerente : public Funcionario{
 
     public:
       /**
-      @brief Método contrutor de um Gerente
-      @param id é o id que deve ser atribuidio ao gerente
-      @param nome é o nome atribuido ao gerente
-      @param s é o supermercado referente
+      *@brief Método contrutor de um Gerente
+      *@param id é o id que deve ser atribuidio ao gerente
+      *@param nome é o nome atribuido ao gerente
+      *@param s é o supermercado referente
       */
       Gerente(string nome, int id, Supermercado *s );
 
       /**
-      @brief Método que retorna o saldo do caixa
-      @return retorna o saldo do caixa
+      *@brief Método que retorna o saldo do caixa
+      *@return retorna o saldo do caixa
       */
       float consulta_saldo_caixa();
 
       /**
-      @brief Atualiza a lista de promoções
+      *@brief Atualiza a lista de promoções
       */
       void atualiza_promocoes();
 
       /**
-      @brief Atualiza o nomero de produtos no estoque
+      *@brief Atualiza o nomero de produtos no estoque
       */
       void atualiza_estoque();
 
       /**
-      @brief Método que imprime a quantidade de cada produto
+      *@brief Método que imprime a quantidade de cada produto
       */
       void ver_qtd_produtos();
 
